35fe7227d02d038b57107404baca4b7277a5b393
RantuchoM <martin.rantucho@gmail.com>
2023-10-24 17:52:11 -0300

    f

--- scripts/project.js
+++ scripts/project.js
@@ -1,52 +1,103 @@
 // Use the JSON data URL you provided
 const jsonDataUrl = 'https://raw.githubusercontent.com/RantuchoM/cse121b/main/jsons/json.json';
 
-const ensembleDropdown = document.getElementById('ensembleDropdown');
+const ensembleCheckboxes = document.getElementById('ensembleCheckboxes');
 const agendaList = document.getElementById('agendaList');
+const startDateInput = document.getElementById('startDate');
+const endDateInput = document.getElementById('endDate');
+const checkAllButton = document.getElementById('checkAllButton');
+const uncheckAllButton = document.getElementById('uncheckAllButton');
+const filterButton = document.getElementById('filterButton');
+
+let data; // Store JSON data
+let ensembleCheckboxesMap = {}; // Map to store ensemble checkboxes
+
+// Function to add triggers to checkboxes
+function addTriggersToCheckboxes() {
+    Object.values(ensembleCheckboxesMap).forEach((checkbox) => {
+        checkbox.addEventListener('change', filterAndDisplayEvents);
+    });
+}
+
+// Function to check all checkboxes
+function checkAll() {
+    Object.values(ensembleCheckboxesMap).forEach((checkbox) => {
+        checkbox.checked = true;
+    });
+    filterAndDisplayEvents(); // Trigger filtering
+}
+
+// Function to uncheck all checkboxes
+function uncheckAll() {
+    Object.values(ensembleCheckboxesMap).forEach((checkbox) => {
+        checkbox.checked = false;
+    });
+    filterAndDisplayEvents(); // Trigger filtering
+}
+
+// Function to filter and display events
+function filterAndDisplayEvents() {
+    // Parse start and end dates or set to null if no value is provided
+    const startDateValue = startDateInput.value;
+    const endDateValue = endDateInput.value;
+    const startDate = startDateValue ? new Date(startDateValue + 'T00:00') : null; // Convert to Date object and set time to midnight
+    const endDate = endDateValue ? new Date(endDateValue + 'T23:59') : null; // Convert to Date object and set time to the end of the day
+
+    // Clear the previous agenda.
+    agendaList.innerHTML = '';
+
+    // Create an array to store the ensembles to filter for this selection.
+    const ensemblesToDisplay = Object.values(ensembleCheckboxesMap)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+
+    // Filter and display events for the selected ensembles and within the date range.
+    data.events.forEach(event => {
+        const eventDate = new Date(event.date + 'T' + event.startTime);
+        const isDateInRange = (!startDate || eventDate >= startDate) && (!endDate || eventDate <= endDate);
+        if (isDateInRange && event.ensembles.some(ensemble => ensemblesToDisplay.includes(ensemble))) {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${event.date}: ${event.startTime} - ${event.endTime}, ${event.location}, Conductor: ${event.conductor}, Repertoire: ${event.repertoire}`;
+            agendaList.appendChild(listItem);
+        }
+    });
+}
 
 // Fetch the JSON data
 fetch(jsonDataUrl)
     .then((response) => response.json())
-    .then((data) => {
-        // Populate the ensemble dropdown with options
+    .then((jsonData) => {
+        data = jsonData; // Store the JSON data for filtering
+
+        // Create checkboxes for ensembles and add to the page
         data.ensembles.forEach((ensemble) => {
-            const option = document.createElement('option');
-            option.value = ensemble.name;
-            option.textContent = ensemble.name;
-            ensembleDropdown.appendChild(option);
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = ensemble.name;
+            checkbox.value = ensemble.name;
+            ensembleCheckboxes.appendChild(checkbox);
+
+            // Store checkboxes in the map
+            ensembleCheckboxesMap[ensemble.name] = checkbox;
+
+            const label = document.createElement('label');
+            label.setAttribute('for', ensemble.name);
+            label.textContent = ensemble.name;
+            ensembleCheckboxes.appendChild(label);
         });
 
-        // Add an event listener to handle dropdown selection
-        ensembleDropdown.addEventListener('change', () => {
-            const selectedEnsemble = ensembleDropdown.value;
+        // Add triggers to checkboxes
+        addTriggersToCheckboxes();
 
-            // Clear the previous agenda.
-            agendaList.innerHTML = '';
+        // Add event listeners to handle button clicks and date input changes
+        checkAllButton.addEventListener('click', checkAll);
+        uncheckAllButton.addEventListener('click', uncheckAll);
+        filterButton.addEventListener('click', filterAndDisplayEvents);
+        startDateInput.addEventListener('input', filterAndDisplayEvents);
+        endDateInput.addEventListener('input', filterAndDisplayEvents);
 
-            // Create an array to store the ensembles to filter for this selection.
-            const ensemblesToDisplay = [selectedEnsemble];
-
-            // Check if the selected ensemble has children.
-            const ensembleData = data.ensembles.find(ensemble => ensemble.name === selectedEnsemble);
-            if (ensembleData && ensembleData.children) {
-                ensemblesToDisplay.push(...ensembleData.children);
-            }
-
-            // Check if the selected ensemble has a parent.
-            const parentEnsembleData = data.ensembles.find(ensemble => ensemble.children && ensemble.children.includes(selectedEnsemble));
-            if (parentEnsembleData) {
-                ensemblesToDisplay.push(parentEnsembleData.name);
-            }
-
-            // Filter and display events for the selected ensembles.
-            data.events.forEach(event => {
-                if (event.ensembles.some(ensemble => ensemblesToDisplay.includes(ensemble))) {
-                    const listItem = document.createElement('li');
-                    listItem.textContent = `${event.date}: ${event.startTime} - ${event.endTime}, ${event.location}, Conductor: ${event.conductor}, Repertoire: ${event.repertoire}`;
-                    agendaList.appendChild(listItem);
-                }
-            });
-        });
+        // Initially display events based on default selections
+        filterAndDisplayEvents();
     })
     .catch((error) => {
         console.error('Error fetching JSON data:', error);
--- w03-Practice/events.js
+++ w03-Practice/events.js
@@ -1,55 +1,64 @@
-let tasks = [];
+// Use the JSON data URL you provided
+const jsonDataUrl = 'https://raw.githubusercontent.com/RantuchoM/cse121b/main/jsons/json.json';
 
-function renderTasks(tasks) {
-  const listElement = document.querySelector("#todoList");
-  listElement.innerHTML = "";
-  tasks.forEach((task) => {
-    listElement.innerHTML += `
-    <li ${task.completed ? 'class="strike"' : ""}>
-      <p>${task.detail}</p>
-      <div>
-        <span data-function="delete">❎</span>
-        <span data-function="complete">✅</span>
-      </div>
-    </li>`;
-  });
-}
+const ensembleDropdown = document.getElementById('ensembleDropdown');
+const agendaList = document.getElementById('agendaList');
+const startDateInput = document.getElementById('startDate');
+const endDateInput = document.getElementById('endDate');
 
-function newTask() {
-  const task = document.querySelector("#todo").value;
-  tasks.push({ detail: task, completed: false });
-  renderTasks(tasks);
-}
+// Fetch the JSON data
+fetch(jsonDataUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        // Populate the ensemble dropdown with options
+        data.ensembles.forEach((ensemble) => {
+            const option = document.createElement('option');
+            option.value = ensemble.name;
+            option.textContent = ensemble.name;
+            ensembleDropdown.appendChild(option);
+        });
 
-function removeTask(taskElement) {
-  tasks = tasks.filter(
-    (task) => task.detail != taskElement.childNodes[1].innerText
-  );
-  taskElement.remove();
-}
+        // Function to filter and display events
+        function filterAndDisplayEvents() {
+            const selectedEnsemble = ensembleDropdown.value;
+            const startDate = new Date(startDateInput.value + 'T00:00'); // Convert to Date object and set time to midnight
+            const endDate = new Date(endDateInput.value + 'T23:59'); // Convert to Date object and set time to the end of the day
 
-function completeTask(taskElement) {
-  const taskIndex = tasks.findIndex(
-    (task) => task.detail === taskElement.childNodes[1].innerText
-  );
-  tasks[taskIndex].completed = tasks[taskIndex].completed ? false : true;
-  taskElement.classList.toggle("strike");
-  console.log(tasks);
-}
+            // Clear the previous agenda.
+            agendaList.innerHTML = '';
 
-function manageTasks(e) {
-  // did they click the delete or complete icon?
-  console.log(e.target);
-  const parent = e.target.closest("li");
-  if (e.target.dataset.function === "delete") {
-    removeTask(parent);
-  }
-  if (e.target.dataset.function === "complete") {
-    completeTask(parent);
-  }
-}
+            // Create an array to store the ensembles to filter for this selection.
+            const ensemblesToDisplay = [selectedEnsemble];
 
-document.querySelector("#submitTask").addEventListener("click", newTask);
-document.querySelector("#todoList").addEventListener("click", manageTasks);
+            // Check if the selected ensemble has children.
+            const ensembleData = data.ensembles.find(ensemble => ensemble.name === selectedEnsemble);
+            if (ensembleData && ensembleData.children) {
+                ensemblesToDisplay.push(...ensembleData.children);
+            }
 
-renderTasks(tasks);
+            // Filter and display events for the selected ensembles and within the date range.
+            data.events.forEach(event => {
+                const eventDate = new Date(event.date + 'T' + event.startTime);
+                if (
+                    eventDate >= startDate &&
+                    eventDate <= endDate &&
+                    event.ensembles.some(ensemble => ensemblesToDisplay.includes(ensemble))
+                ) {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = `${event.date}: ${event.startTime} - ${event.endTime}, ${event.location}, Conductor: ${event.conductor}, Repertoire: ${event.repertoire}`;
+                    agendaList.appendChild(listItem);
+                }
+            });
+        }
+
+        // Add event listeners to handle dropdown and date input changes
+        ensembleDropdown.addEventListener('change', filterAndDisplayEvents);
+        startDateInput.addEventListener('input', filterAndDisplayEvents);
+        endDateInput.addEventListener('input', filterAndDisplayEvents);
+
+        // Initially display events based on default selections
+        filterAndDisplayEvents();
+    })
+    .catch((error) => {
+        console.error('Error fetching JSON data:', error);
+    });
