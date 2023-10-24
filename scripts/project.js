f5cb4e57d81a036c274dd820d066751875362c7c
RantuchoM <martin.rantucho@gmail.com>
2023-10-24 16:52:47 -0300

    f

--- scripts/project.js
+++ scripts/project.js
@@ -1,31 +1,40 @@
-// Sample JSON data URL, you can replace it with the actual URL.
+// Use the JSON data URL you provided
 const jsonDataUrl = 'https://raw.githubusercontent.com/RantuchoM/cse121b/main/jsons/json.json';
 
-// Fetch data from the JSON URL and populate the dropdown.
-fetch(jsonDataUrl)
-    .then(response => response.json())
-    .then(data => {
-        const ensembleDropdown = document.getElementById('ensembleDropdown');
-        const agendaList = document.getElementById('agendaList');
+const ensembleDropdown = document.getElementById('ensembleDropdown');
+const agendaList = document.getElementById('agendaList');
 
-        // Populate the dropdown with ensemble names from JSON data.
-        data.ensembles.forEach(ensemble => {
+// Fetch the JSON data
+fetch(jsonDataUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        // Populate the ensemble dropdown with options
+        data.ensembles.forEach((ensemble) => {
             const option = document.createElement('option');
             option.value = ensemble.name;
             option.textContent = ensemble.name;
             ensembleDropdown.appendChild(option);
         });
 
-        // Handle dropdown change event.
+        // Add an event listener to handle dropdown selection
         ensembleDropdown.addEventListener('change', () => {
             const selectedEnsemble = ensembleDropdown.value;
 
             // Clear the previous agenda.
             agendaList.innerHTML = '';
 
-            // Filter and display events for the selected ensemble.
+            // Create an array to store the ensembles to filter for this selection.
+            const ensemblesToDisplay = [selectedEnsemble];
+
+            // Check if the selected ensemble has children.
+            const ensembleData = data.ensembles.find(ensemble => ensemble.name === selectedEnsemble);
+            if (ensembleData && ensembleData.children) {
+                ensemblesToDisplay.push(...ensembleData.children);
+            }
+
+            // Filter and display events for the selected ensembles.
             data.events.forEach(event => {
-                if (event.ensembles.includes(selectedEnsemble)) {
+                if (event.ensembles.some(ensemble => ensemblesToDisplay.includes(ensemble))) {
                     const listItem = document.createElement('li');
                     listItem.textContent = `${event.date}: ${event.startTime} - ${event.endTime}, ${event.location}, Conductor: ${event.conductor}, Repertoire: ${event.repertoire}`;
                     agendaList.appendChild(listItem);
@@ -33,6 +42,6 @@ fetch(jsonDataUrl)
             });
         });
     })
-    .catch(error => {
+    .catch((error) => {
         console.error('Error fetching JSON data:', error);
     });
